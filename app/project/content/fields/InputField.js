dfd74dd4199d9613903339318efc57e30dc9c309
Barrett Alexander <barrett@clockwork.net>
2016-04-26 08:37:41 -0500

    Updated input field to include focus

--- app/project/content/fields/InputField.js
+++ app/project/content/fields/InputField.js
@@ -3,11 +3,33 @@ import React, { Component, PropTypes } from 'react';
 import styles from './InputField.css';
 
 export default class Inputfield extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+  }
+
+
   static get PropTypes() {
     return {
       definition: PropTypes.object.isRequired,
       onValueChange: PropTypes.func.isRequired,
+      onFocus: PropTypes.func,
+      onBlur: PropTypes.func,
     }
+  }
+
+
+  handleFocus(e) {
+    this.setState({active: true});
+    this.props.onFocus && this.props.onFocus(e);
+  }
+
+
+  handleBlur(e) {
+    this.setState({active: false});
+    this.props.onBlur && this.props.onBlur(e);
   }
 
 
@@ -29,9 +51,11 @@ export default class Inputfield extends Component {
 
     const calculatedValue = typeof value !== 'undefined' ? value : defaultValue;
 
+    let mode = this.state.active ? styles.active : styles.inactive;
+
     return (
-      <div className={styles.inputField}>
-        <input className={styles.input} name={name} id={name} type={type} value={calculatedValue} {...attributes} onChange={this.handleValueChange.bind(this)}/>
+      <div className={`${styles.inputField} ${mode}`}>
+        <input className={styles.input} name={name} id={name} type={type} value={calculatedValue} {...attributes} onChange={this.handleValueChange.bind(this)} onFocus={this.handleFocus.bind(this)} onBlur={this.handleBlur.bind(this)}/>
         <label className={styles.label} htmlFor={name}>{label}</label>
       </div>
     );
