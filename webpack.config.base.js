801b6c7e789519595ac8fc4bcfb755466f27d02f
Barrett Alexander <barrett@clockwork.net>
2016-04-27 18:15:36 -0500

    Updated build

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -42,7 +42,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     libraryTarget: 'commonjs2'
   },
